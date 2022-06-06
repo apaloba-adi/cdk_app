99810cd6507cfd8669e09a56d1a5350fb259912b
Ali Paloba <ali.paloba@analog.com>
2022-06-06 13:46:21 -0400

    created logbucket and added automatic log deployment

--- cdk_app/cdk_app_stack.py
+++ cdk_app/cdk_app_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    aws_s3 as s3
+    aws_s3 as s3,
+    aws_s3_assets as s3_assets,
+    aws_s3_deployment as s3_deploy
 )
 from constructs import Construct
 
@@ -10,5 +12,8 @@ class CdkAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        bucket = s3.Bucket(self, "LogBucket", block_public_access=True, versioned=True)
-        
+        bucket = s3.Bucket(self, "LogBucket", versioned=True, block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+
+        s3_deploy.BucketDeployment(self, "DeployLog",
+            sources=[s3_deploy.Source.asset("/Users/apaloba/Library/Application Support/Google/Chrome/", exclude=["**", "!chrome_debug.log"])],
+            destination_bucket=bucket)
